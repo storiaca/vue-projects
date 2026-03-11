ff36d964633b67c7da6fc64bc7a9c1dc55495e18
Aleksandar Ristic <storiaca@gmail.com>
2026-03-11 14:37:16 +0100

    Workout page

--- vuejs-full-course/src/App.vue
+++ vuejs-full-course/src/App.vue
@@ -4,7 +4,7 @@ import Layout from "./components/layouts/Layout.vue"
 import Dashboard from "./components/pages/Dashboard.vue"
 import Workout from "./components/pages/Workout.vue"
 
-const selectDisplay = 2
+const selectDisplay = 3
 </script>
 
 <template>
@@ -14,7 +14,7 @@ const selectDisplay = 2
     <!-- PAGE 2 -->
     <Dashboard v-if="selectDisplay == 2"/>
     <!-- PAGE 3 -->
-     <Workout v-if="selectDisplay == 3"/>
+    <Workout v-if="selectDisplay == 3"/>
   </Layout>
 </template>
 
