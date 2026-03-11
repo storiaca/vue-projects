2859194a8bca737ded28fbe2768da012156a986f
Aleksandar Ristic <storiaca@gmail.com>
2026-03-11 09:52:10 +0100

    Layout comopnent

--- vuejs-full-course/src/App.vue
+++ vuejs-full-course/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Welcome from './components/pages/Welcome.vue';
+import Layout from "./components/layouts/Layout.vue"
+import Dashboard from "./components/pages/Dashboard.vue"
 </script>
 
 <template>
-  <div>
-    <h1 class="text-gradient">Hello World</h1>
+  <Layout>
     <Welcome />
-  </div>
+    <Dashboard />
+  </Layout>
 </template>
 
 <style scoped>
